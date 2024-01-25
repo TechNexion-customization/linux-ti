c1225e64baaa54c6740093adfaf6678bdb866155
Richard Hu <richard.hu@technexion.com>
2024-01-25 14:58:46 +0800

    arm: drm: bridge: TI DS90UB941/DS90UB948: improve indent

--- drivers/gpu/drm/bridge/ds90ub94x.c
+++ drivers/gpu/drm/bridge/ds90ub94x.c
@@ -37,7 +37,7 @@ static void ds90ub94x_display_setting(void)
         printk("------------>%s, %s\r\n", __FILE__, __FUNCTION__);
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x01, 0x0f); /* Reset DSI/DIGITLE */
 	usleep_range(5000, 6000);	/* time cannot be too short */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x03, 0xBA); /* Enable FPD-Link I2C pass through */
+    ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x03, 0xBA); /* Enable FPD-Link I2C pass through */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x1E, 0x01); /* Select FPD-Link III Port 0 */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x5B, 0x21); /* FPD3_TX_MODE=single, Reset PLL */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x4F, 0x8C); /* DSI Continuous Clock Mode,DSI 4 lanes */
@@ -49,42 +49,42 @@ static void ds90ub94x_display_setting(void)
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x01, 0x01); /* ds90ub948 reset */
 	usleep_range(10000, 11000);	/* time cannot be too short */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x49, 0x62); /* Set FPD_TX_MODE, MAPSEL=1(SPWG), Single OLDI output */
-        /* Set GPIO via local I2C */
-        //ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x01); /* Select FPD-Link III Port 0, GPIOx instead of D_GPIOx */
+	/* Set GPIO via local I2C */
+	//ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x01); /* Select FPD-Link III Port 0, GPIOx instead of D_GPIOx */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x02); /* Select FPD-Link III Port 0, GPIOx instead of D_GPIOx */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1D, 0x19); /* GPIO0, MIPI_BL_EN */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1E, 0x99); /* GPIO1, MIPI_VDDEN; GPIO2, MIPI_BL_PWM */
 
-        /* Configure remote devcie ID in local SER */
-        #if 0
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0xac); /* SlaveID_0: EEPROM */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0xac); /* SlaveAlias_0: EEPROM */
+	/* Configure remote devcie ID in local SER */
+	#if 0
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0xac); /* SlaveID_0: EEPROM */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0xac); /* SlaveAlias_0: EEPROM */
 
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x8, 0xac); /* SlaveID_0: EEPROM */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x10, 0xac); /* SlaveAlias_0: EEPROM */
 	#endif
 
-        //Reset touch interrupt
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x02);
-        //msleep(10);
-        //ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1f, 0x01);
-        //msleep(10);
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1f, 0x09);
-        msleep(10);
+	//Reset touch interrupt
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x02);
+	//msleep(10);
+	//ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1f, 0x01);
+	//msleep(10);
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1f, 0x09);
+	msleep(10);
 
-        /* exc80w46 */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0x54); /* SlaveID_0: touch panel */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0x54); /* SlaveAlias_0: touch panel */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x8, 0x54); /* SlaveID_0: touch panel */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x10, 0x54); /* SlaveAlias_0: touch panel */
+	/* exc80w46 */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0x54); /* SlaveID_0: touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0x54); /* SlaveAlias_0: touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x8, 0x54); /* SlaveID_0: touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x10, 0x54); /* SlaveAlias_0: touch panel */
 #if 0
-        /* edt-ft5x06 */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0x70); /* SlaveID_0: touch panel */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0x70); /* SlaveAlias_0: touch panel */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x8, 0x70); /* SlaveID_0: touch panel */
-        ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x10, 0x70); /* SlaveAlias_0: touch panel */
+	/* edt-ft5x06 */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0x70); /* SlaveID_0: touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0x70); /* SlaveAlias_0: touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x8, 0x70); /* SlaveID_0: touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x10, 0x70); /* SlaveAlias_0: touch panel */
 #endif
-        /* Configure remote interrupt for touch panel */
+	/* Configure remote interrupt for touch panel */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x30, 0x1); /* REM_INTB_CTRL: port 0 remote interrupt */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0xC6, 0x21); /* INTB: enable INTB_IN on remote DES */
 }
